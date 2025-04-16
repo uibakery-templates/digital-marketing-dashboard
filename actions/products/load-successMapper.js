b46f4eff6a65f438c91d958e670258b9e1e3ab5e
UI Bakery <template.maker@uibakery.io>
2025-04-16 08:41:36 +0000

    fix: don't highlight any text when search is empty

--- actions/products/load-successMapper.js
+++ actions/products/load-successMapper.js
@@ -1,4 +1,15 @@
 const search = {{ui.searchInput.value}};
+
+const highlight = (text, search) => {
+  if (!search) {
+    return text;
+  }
+  return text.replace(new RegExp(search, 'ig'), '<b style="background-color:Tomato;padding: 0 5px; color: white">' + search + '</b>');
+};
+
 return {{data}}.map((item) => {
-  return { ...item, productName: item.productName.replace(new RegExp(search, 'ig'), '<b style="background-color:Tomato;padding: 0 5px; color: white">' + search + '</b>') };
+  return {
+    ...item,
+    productName: highlight(item.productName, search)
+  };
 });
